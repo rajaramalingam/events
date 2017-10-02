3b1a07513365fba6cafff98bdca2ef9de36c1b61
rajaramalingam <rajaramalingam345@gmail.com>
2017-10-02 18:31:52 +0530

    1cmt

---
+++ event1.js
@@ -0,0 +1,36 @@
+function myEvent() {
+
+ 	 document.getElementById("demo").innerHTML = "Have a Nice Day";
+
+ }
+
+ function myEvent1() {
+
+ 	 document.getElementById("demo1").innerHTML = "Have a Good Day";
+
+ }
+ function myEvent2() {
+    document.getElementById("demo2").style.backgroundColor = "red";
+ }
+
+
+function myEvent3(){
+	var a=document.getElementById("qsn").value;
+	// alert("Good Morning");
+	document.getElementById("ans").innerHTML=a;
+}
+
+function myEvent4(){
+	
+	// alert("Good Evening");
+	
+}
+
+function myEvent5(){
+	 document.getElementById("demo5").style.backgroundColor = "green";
+}
+
+
+function myEvent6(){
+	alert("Hello Moto");
+}
